92543706d8fe950ac683e1b5c625dcc1686a3a5a
Alberto Gomez <alberto.gomez@kcl.ac.uk>
2020-06-08 17:56:14 +0100

    issue #2: added block anonymization

---
+++ Anonymization/anonymize_video_blackblock.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# usage: bash anonymize_video_blackblock.sh <videoname in> <videoname out> <xmin xmax ymin ymax>
+# example:
+# bash ~/repos/VITAL/echocardiography/Anonymization/anonymize_video.sh 01NVb_sample_short.avi 01NVb_sample_test1_anonym.mp4 300 1550 60 850
+
+# bounding box xmin xmax ymin ymax
+#bb=(300 1550 60 850)
+bb=($3 $4 $5 $6)
+h=` echo $6-$5 | bc`
+w=` echo $4-$3 | bc`
+
+# Get video height and width
+array=`ffprobe -v error -show_entries stream=width,height -of csv=p=0:s=x $1 | awk 'BEGIN{ FS="x"}{print $1" "$2}'`
+IFS=' ' read -r -a imsize <<< "$array"
+echo ${imsize[1]}
+
+ffmpeg -y -i $1 -vf "drawbox=x=${bb[0]}:y=${bb[2]}:w=$w:h=$h:color=black:t=fill"  $2
