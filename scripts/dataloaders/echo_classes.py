40fdbbb97edeca8422c10e481f3a134dc65ab6d7
Alberto Gomez <alberto.gomez@kcl.ac.uk>
2022-02-08 12:31:00 +0000

    issue #37: adde the scaffold for the pretus plugin- not working yet

--- scripts/dataloaders/echo_classes.py
+++ scripts/dataloaders/echo_classes.py
@@ -2,6 +2,7 @@ import argparse
 
 import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
 import torchvision.transforms as transforms
 import yaml
 
@@ -77,3 +78,47 @@ if __name__ == '__main__':
             subplot_index +=1
 
     plt.show()
+
+    # TODO in #34
+    training = False
+    if training == True:
+        # Dataloader that will serve the batches over the epochs
+        dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=config['batch_size'], shuffle=True)
+
+        # Do a loop as if we were training a model
+        data_size = tuple(data_idx[0].shape)
+        print(type(data_size))
+        net = SimpleVideoClassifier(data_size)
+        net.to(device)
+        print(net)
+
+        optimizer = torch.optim.Adam(net.parameters()) # use default settings
+        loss_function = nn.CrossEntropyLoss()
+
+        losses = []
+        for epoch in range(config['max_epochs']):
+            running_loss = 0
+            for step, data in enumerate(dataloader):
+                clip = data[0]
+                label = data[1].to(device)
+
+                out = net(clip)
+
+                loss = loss_function(out, label)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.detach().item()
+
+            running_loss /= len(dataloader)
+            losses.append(running_loss)
+
+            print('{:03.0f} {:.5f}'.format(epoch, running_loss))
+
+        plt.figure()
+        plt.plot(losses,'-')
+        plt.xlabel('Epochs')
+        plt.ylabel('Cross Entropy Loss')
+        plt.title('Training')
+        plt.show()
