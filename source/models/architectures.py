33b579d033d43d07f8f9855d75fa4492981bc5bf
mxochicale <miguel.xochicale@kcl.ac.uk>
2022-08-12 23:07:43 +0100

    adds working/tested EfficientNet_source0 (#49)

--- source/models/architectures.py
+++ source/models/architectures.py
@@ -7,6 +7,86 @@ import torch.nn.functional as F
 import torch.nn.init as init
 
 
+class Block(nn.Module):
+    '''expand + depthwise + pointwise + squeeze-excitation'''
+
+    def __init__(self, in_planes, out_planes, expansion, stride):
+        super(Block, self).__init__()
+        self.stride = stride
+
+        planes = expansion * in_planes
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, groups=planes, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(
+            planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes)
+
+        self.shortcut = nn.Sequential()
+        if stride == 1 and in_planes != out_planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1,
+                          stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(out_planes),
+            )
+
+        # SE layers
+        self.fc1 = nn.Conv2d(out_planes, out_planes // 16, kernel_size=1)
+        self.fc2 = nn.Conv2d(out_planes // 16, out_planes, kernel_size=1)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        shortcut = self.shortcut(x) if self.stride == 1 else out
+        # Squeeze-Excitation
+        w = F.avg_pool2d(out, out.size(2))
+        w = F.relu(self.fc1(w))
+        w = self.fc2(w).sigmoid()
+        out = out * w + shortcut
+        return out
+
+
+class EfficientNet_source0(nn.Module):
+    """
+    https://github.com/TommyLitlle/AS-OCT-AFSNet/blob/main/efficientnet.py
+    Paper: "EfficientNet: Rethinking Model Scaling for Convolutional Neural Networks".
+    https://arxiv.org/abs/1905.11946?context=stat.ML
+    https://github.com/tensorflow/tpu/tree/master/models/official/efficientnet
+    """
+
+    def __init__(self, cfg, num_classes: int = 3, in_channels: int = 3):
+        super(EfficientNet_source0, self).__init__()
+        self.cfg = cfg
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.layers = self._make_layers(in_planes=32)
+        self.avg_pool2d = nn.AdaptiveAvgPool2d(1)
+        self.linear = nn.Linear(cfg[-1][1], num_classes)
+
+    def _make_layers(self, in_planes):
+        layers = []
+        for expansion, out_planes, num_blocks, stride in self.cfg:
+            strides = [stride] + [1] * (num_blocks - 1)
+            for stride in strides:
+                layers.append(Block(in_planes, out_planes, expansion, stride))
+                in_planes = out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = torch.squeeze(x, dim=1)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.layers(x)
+        x = self.avg_pool2d(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
+
+
 class fire_(nn.Module):
     def __init__(self, inplanes, squeeze_planes, expand_planes):
         super(fire_, self).__init__()
